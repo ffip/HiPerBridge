f815f46869b903c22524db8e64c686b8d36a055b
Cato Sweeney <cato@the.bb>
2022-11-07 14:48:07 +0800

    #50 日志类型判断问题修复

--- src/log_parser.rs
+++ src/log_parser.rs
@@ -35,7 +35,7 @@ pub fn try_get_ipv4(line: &str) -> Option<String> {
 
 pub fn try_get_valid(line: &str) -> Option<String> {
     if let Ok(JsonValue::Object(log_data)) = line.parse::<JsonValue>() {
-        if let Some(JsonValue::Object(valid_data)) = log_data.get("valid") {
+        if let Some(JsonValue::String(valid_data)) = log_data.get("valid") {
             return Some(valid_data.to_owned());
         }
     }
