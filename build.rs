355ff429e4b49f463cabc868cd04896004021536
Sabrina Baker <115603041+ffip@users.noreply.github.com>
2024-01-08 09:23:18 +0000

    优化提权逻辑

--- build.rs
+++ build.rs
@@ -2,9 +2,6 @@
 fn main() {
     use winres::*;
     let mut res = WindowsResource::new();
-    res.set_icon("assets/icon-slim.ico");
-    res.set_icon_with_id("assets/icon-slim.ico", "ICON");
-    res.set_icon_with_id("assets/icon-slim-gray.ico", "ICON_GRAY");
     #[cfg(not(debug_assertions))]
     res.set_manifest_file("assets/manifest.xml"); // 在这里设置默认管理员权限
     res.compile().unwrap();
